0663ef291462b413c43cd1fd9ebfa604874d18cd
CREDITMITRAA <developers@creditmitra.in>
2025-01-10 10:42:32 +0530

    117 : added created_by and updated_by fields in LeadDocument Model

--- models/leadDocument.js
+++ models/leadDocument.js
@@ -1,12 +1,21 @@
-const {DataTypes} = require('sequelize')
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    return sequelize.define('LeadDocument',{
-        id:{type: DataTypes.INTEGER, autoIncrement:true, primaryKey:true},
-        lead_id:{ type: DataTypes.INTEGER, allowNull: false },
-        document_url : { type: DataTypes.STRING },
-        document_type : { type: DataTypes.STRING },
-        document_name : { type: DataTypes.STRING },
-        status:{ type: DataTypes.ENUM('active', 'inactive', 'deleted'), defaultValue: 'active' },
-    },{ timestamps: true })
-}
+  return sequelize.define(
+    "LeadDocument",
+    {
+      id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+      lead_id: { type: DataTypes.INTEGER, allowNull: false },
+      document_url: { type: DataTypes.STRING },
+      document_type: { type: DataTypes.STRING },
+      document_name: { type: DataTypes.STRING },
+      updated_by: { type: DataTypes.INTEGER },
+      created_by: { type: DataTypes.INTEGER },
+      status: {
+        type: DataTypes.ENUM("active", "inactive", "deleted"),
+        defaultValue: "active",
+      },
+    },
+    { timestamps: true }
+  );
+};

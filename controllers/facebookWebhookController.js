c809345080618903d59153662c975b1572328dbf
CREDITMITRAA <developers@creditmitra.in>
2025-08-11 17:30:36 +0530

    misc

--- controllers/facebookWebhookController.js
+++ controllers/facebookWebhookController.js
@@ -3,7 +3,8 @@ const { ApiResponse } = require('../utilities/api-responses/ApiResponse');
 
 exports.verifyWebhook = (req, res) => {
   const VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
-
+  console.log('token received = ', VERIFY_TOKEN);
+  
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];

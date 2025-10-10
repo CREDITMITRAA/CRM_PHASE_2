d1cbc0e92349695e9f2d3c811d88434bce949ac6
CREDITMITRAA <developers@creditmitra.in>
2025-10-10 14:49:58 +0530

    s3 from fileUPloadController imported was wrong

--- controllers/filesUploadController.js
+++ controllers/filesUploadController.js
@@ -7,7 +7,7 @@ const { addLeadDocument } = require("../services/leadDocumentServices");
 const { createLogData, createActivityLog } = require("../services/ActivityLogServices");
 const { ACTIVITY_LOGS, ACTIVITY_TYPES } = require("../utilities/ActivityLogConstants");
 
-export const s3 = new AWS.S3();
+const s3 = new AWS.S3();
 
 // const upload = multer({ dest: "uploads/" });
 
@@ -211,5 +211,6 @@ async function uploadMultipleFiles(req, res) {
 
 module.exports = {
   uploadFile,
-  uploadMultipleFiles
+  uploadMultipleFiles,
+  s3
 };
